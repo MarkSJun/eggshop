1b6e17a589cebe7f09f2392616d1807d87879ba7
小马大哥哥 <markshanjun@gmail.com>
2021-07-09 16:50:12 +0800

    test

--- config/config.default.js
+++ config/config.default.js
@@ -50,7 +50,7 @@ module.exports = appInfo => {
     host: '127.0.0.1',
     port: 3306,
     username: "root",
-    password: "123456",
+    password: "root",
     database: 'eggshop',
   };
   return {
